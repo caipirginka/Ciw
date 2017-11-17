7056c1416a18b2cd83cae5627a0668037b39024d
caipirginka <github@oicom.com>
2017-11-17 13:02:28 +0100

    Tried again to use a TimeDependent function for arrivals too, but couldn't get it work.

--- prove/prova2.py
+++ prove/prova2.py
@@ -84,10 +84,13 @@ def create_arrival_func(k_category):
     tick = ticks.get(k_category,None)
     
     def _func(t):
-        print 'arrival {}'.format(t)
-        res = tick.get(t,0) if tick else duetime
-        if not res:
-            res = duetime if t > tick.keys()[-1] else 1
+        print 'arrival {}: {}'.format(t,k_category)
+        res = 1
+        if t:
+            res = tick.get(t,1) - 1 if tick else duetime
+            if not res:
+                #res = duetime if t > tick.keys()[-1] else 1
+                res = duetime
         res = res * 1.0            #ensure float
         if res and True:
             print 'arrival {}: {} => {}'.format(t,k_category,res)
@@ -99,8 +102,12 @@ def create_batch_func(k_category):
 
     def _func(t):
         #print 'batch {}'.format(t)
-        res = piece.get(t,0) * 1.0 if piece else 0.0
-        #res = piece.get((t or 0) + 1,0) * 1.0 if piece else 0.0
+        res = 0
+        if t:
+            #res = piece.get(t,0) if piece else 0
+            #res = piece.get(t + 1,0) if piece else 0
+            res = piece.get(t,0) if piece else 0
+        res = res * 1.0            #ensure float
         if res and True:
             print 'batch {}: {} => {}'.format(t,k_category,res)
         return res
@@ -134,13 +141,13 @@ for k_category,v_category in categories.iteritems():
     i_node = 0
     for k_node,v_node in nodes.iteritems():
         #print k_node
-        arrivals[clss].append(['Deterministic', 1.0])
+        #arrivals[clss].append(['Deterministic', 1.0])
         if v_category['node'] == k_node:
-            #arrivals[clss].append(['TimeDependent', create_arrival_func(k_category)])
+            arrivals[clss].append(['TimeDependent', create_arrival_func(k_category)])
             services[clss].append(['Deterministic', v_category['time'] * 1.0])
             batches[clss].append(['TimeDependent', create_batch_func(k_category)])
         else:            
-            #arrivals[clss].append(['TimeDependent', one_func])
+            arrivals[clss].append(['TimeDependent', one_func])
             services[clss].append(['Deterministic', 0.0])
             batches[clss].append(['TimeDependent', zero_func])
         i_node = i_node + 1
@@ -164,6 +171,8 @@ for order in orders:
             duetime = order['duetime']
         #print piece[t]
 
+begintime = begintime - 1
+
 if begintime != 0:
     duetime = duetime - begintime
     for piece in pieces.values():
@@ -173,9 +182,9 @@ if begintime != 0:
 
 for k_tick,v_tick in ticks.iteritems():
     piece = sorted(pieces.get(k_tick,[]))
-    prev = 0.0
+    prev = 1.0
     for tick in piece:
-        v_tick[prev] = tick - prev - 1
+        v_tick[prev] = tick - prev + 1
         prev = tick
     
 print arrivals
@@ -211,7 +220,7 @@ Q = ciw.Simulation(N)
 
 t4 = timeit.default_timer()
 
-Q.simulate_until_max_time(duetime + 1)
+Q.simulate_until_max_time(duetime + 20)
 #Q.simulate_until_max_customers(total, method='Arrive')
 
 t5 = timeit.default_timer()
